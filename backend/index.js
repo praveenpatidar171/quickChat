5dd8239258e58f6b9ee1e13a13d8d40fbd8a1a0a
Praveen <praveenpatidar171@gmail.com>
2025-01-02 08:15:00 +0530

    updates cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const { app, server } = require('./socket/socket');
 const _dirname = path.resolve();
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5173',
+    origin: 'https://quickchat-kgb4.onrender.com',
     credentials: true,
 }));
 app.use(express.json());
